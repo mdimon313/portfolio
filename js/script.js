4a7f2ca74874c30716fc3625a8b3ea3c194210c7
Mohammd Imon <imon26362@gmail.com>
2020-12-20 19:27:23 +0600

    replace

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let showBtn = document.querySelector(".show-btn");
+let closeBtn = document.querySelector(".close-btn");
+let navList = document.querySelector("#nav-list");
+
+showBtn.addEventListener('click', ()=> {
+    navList.style = 'left: 0';
+    closeBtn.style.display = 'block';
+    showBtn.style.display = 'none';
+})
+closeBtn.addEventListener("click", () => {
+    navList.style = "left: -100%";
+    closeBtn.style.display = "none";
+    showBtn.style.display = "block";
+});
